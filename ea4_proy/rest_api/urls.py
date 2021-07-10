98f669b328a0e436b7ea631641c51b6898b90acf
victor vargas <vict.vargass@duocuc.cl>
2021-07-09 22:04:25 -0400

    login de viewsLogin importado y con path creada

--- ea4_proy/rest_api/urls.py
+++ ea4_proy/rest_api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from rest_api.views import obra_api
+from rest_api.views import obras_api, obra_api
+from rest_api.viewsLogin import login
 
 urlpatterns = [
-    path('obra-api', obra_api, name='obra'),
+    path('obras-api', obras_api, name='obras'),
+    path('obra/<pk>', obra_api, name='obra'),
+    path('login', login, name='login'),
 ]
