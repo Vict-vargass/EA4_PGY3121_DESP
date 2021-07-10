4a1fe9b56c049b7cb5b93f7551e4a78e656c2bae
victor vargas <vict.vargass@duocuc.cl>
2021-07-09 22:03:35 -0400

    importaciones para implementar seguridad con token

--- ea4_proy/rest_api/views.py
+++ ea4_proy/rest_api/views.py
@@ -3,11 +3,14 @@ from core_app.models import Obra
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-
+# agregar el decorador sobre los def
+@permission_classes((IsAuthenticated,))
 @api_view(['GET', 'POST'])
-def obra_api (request):
+def obras_api (request):
     if request.method=='GET':
         lista_obra = Obra.objects.all()
         serializer = ObraSerializer(lista_obra, many=True)
@@ -22,3 +25,29 @@ def obra_api (request):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@permission_classes((IsAuthenticated,))
+@api_view(['GET', 'PUT', 'DELETE'])
+def obra_api(request, pk):
+    try:
+        obra = Obra.objects.get(idObra=pk)
+    except Obra.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = ObraSerializer(obra)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ObraSerializer(obra, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+
+        obra.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    else:
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
